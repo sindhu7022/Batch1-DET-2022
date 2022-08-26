475797c0f0a7f68540fc9026067bf218da464474
sindhu7022 <76683190+sindhu7022@users.noreply.github.com>
2022-08-26 16:38:10 +0530

    Add files via upload
    today task

---
+++ 26 Aug/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Employee
+    {
+        int rollno;
+        string ename;
+        string eadd;
+        int pincode;
+        int phno;
+        float GSalary;
+        float pf;
+
+        public Employee(int rollno, string ename, string eadd, int pincode, int phno)
+        {
+            this.rollno = rollno;
+            this.ename = ename;
+            this.eadd = eadd;
+            this.pincode = pincode;
+            this.phno = phno;
+            this.pf = pf;
+            this.GSalary = GSalary;
+        }
+
+        public float GetNetSalary()
+        {
+            return (GSalary - pf);
+        }
+
+        public string GetGrade()
+        {
+            float net = GetNetSalary();
+            if (net > 10000)
+                return "A";
+            else if (net > 5000 && net < 10000)
+                return "B";
+            else
+                return "C";
+        }
+    }
+}
---
+++ 26 Aug/circle.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleApp5
+//{
+//    internal class circle
+//    {
+//        double radius;
+
+
+//        public circle(double radius)
+//        {
+//            this.radius = radius;
+//        }
+//        public double area()
+//        {
+//            return Math.PI * radius * radius;
+//        }
+//    }
+//}
