363ee898b3b0890186baf81940ba7ae675684f9d
sindhu <sindhuc8296@gmail.com>
2022-08-26 18:29:36 +0530

    changes are done

---
+++ Batch1-DET-2022/TestEmployee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal class TestEmployee
+    {
+    
+        public static void Main()
+        {
+            Employee ceo = new Employee(22854, "sindhu", "RCR", 741852963, 50252);
+            float NET = ceo.GetNetSalary();
+            string GRADE = ceo.GetGrade();
+            Console.WriteLine($"Net Salary = {NET}, Grade = {GRADE}");
+        }
+    }
+}
